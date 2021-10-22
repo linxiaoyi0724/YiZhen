d4210411b7e3a06a1a50e3d790e55d74f43dd442
林轶锽（Lin Yihuang）(linyihuang) <linyihuang@qiyi.com>
2021-10-22 23:29:55 +0800

    Initial Commit

---
+++ YiZhen/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  YiZhen
+//
+//  Created by linxiaoyi on 2021/10/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ YiZhen/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  YiZhen
+//
+//  Created by linxiaoyi on 2021/10/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
