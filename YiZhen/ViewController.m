a00f45793066108cddd7265c83ddebc2da3c21b6
林轶锽（Lin Yihuang）(linyihuang) <linyihuang@qiyi.com>
2021-10-29 00:56:51 +0800

    【Feature】获取手机系统相册照片并显示

--- YiZhen/ViewController.m
+++ YiZhen/ViewController.m
@@ -7,7 +7,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 @property (nonatomic, strong) UIImageView* mImageView;
 @end
 
@@ -16,11 +16,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 20, 280, 250)];
-    imageView.contentMode = UIViewContentModeScaleAspectFill;
-    [self.view addSubview:imageView];
-    self.mImageView = imageView;
-    [self onCustom];
+    self.mImageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 20, 280, 280)];
+    _mImageView.backgroundColor = [UIColor yellowColor];
+    _mImageView.layer.cornerRadius = 50;
+    _mImageView.layer.masksToBounds = YES;
+    _mImageView.tag = 101;
+    [self.view addSubview:_mImageView];
+    
+    _mImageView.userInteractionEnabled = YES;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction:)];
+    [_mImageView addGestureRecognizer:tap];
+    
+}
+
+- (void) tapAction:(UIImageView* )sender{
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
+    imagePicker.allowsEditing = YES;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePicker.delegate = self;
+    [self presentViewController:imagePicker animated:YES completion:^{NSLog(@"打开相册");}];
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [picker dismissViewControllerAnimated:YES completion:^{NSLog(@"取消");}];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    _mImageView.image = info[UIImagePickerControllerEditedImage];
+    NSLog(@"%@",info);
+    [picker dismissViewControllerAnimated:YES completion:^{NSLog(@"选照片");}];
 }
 
 - (void) onCustom{
